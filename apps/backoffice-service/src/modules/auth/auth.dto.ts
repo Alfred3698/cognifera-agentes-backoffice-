bedd91374d1bc21d272c1a81d5d4ebde579911b0
Alfred <jajimenez@baufest.com>
2025-04-23 23:12:46 -0600

    se agrega anotación swagger para generar token

--- apps/backoffice-service/src/modules/auth/auth.dto.ts
+++ apps/backoffice-service/src/modules/auth/auth.dto.ts
@@ -1,8 +1,18 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UsersDto {
+  @ApiProperty({
+    description: 'Nombre de usuario',
+    example: 'john_doe',
+  })
   @IsString()
   userName: string;
+
+  @ApiProperty({
+    description: 'Contraseña del usuario',
+    example: 'password123',
+  })
   @IsString()
   password: string;
 }
