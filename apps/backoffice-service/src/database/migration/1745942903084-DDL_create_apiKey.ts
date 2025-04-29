e62afb5883c8c462ca9a19e6e725427b2d1db98d
Alfred <jajimenez@baufest.com>
2025-04-29 10:52:05 -0600

    se actualiza migración

--- apps/backoffice-service/src/database/migration/1745942903084-DDL_create_apiKey.ts
+++ apps/backoffice-service/src/database/migration/1745942903084-DDL_create_apiKey.ts
@@ -7,7 +7,7 @@ export class DDLCreateApiKey1745942903084 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "api_keys" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "key" character varying NOT NULL, "created_at" TIMESTAMP DEFAULT now(), "user_id" uuid, CONSTRAINT "UQ_e42cf55faeafdcce01a82d24849" UNIQUE ("key"), CONSTRAINT "PK_5c8a79801b44bd27b79228e1dad" PRIMARY KEY ("id"))`,
     );
-    await queryRunner.query(`ALTER TABLE "test_me" DROP COLUMN "nombre"`);
+
     await queryRunner.query(
       `ALTER TABLE "api_keys" ADD CONSTRAINT "FK_a3baee01d8408cd3c0f89a9a973" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
@@ -17,9 +17,7 @@ export class DDLCreateApiKey1745942903084 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "api_keys" DROP CONSTRAINT "FK_a3baee01d8408cd3c0f89a9a973"`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "test_me" ADD "nombre" character varying`,
-    );
+
     await queryRunner.query(`DROP TABLE "api_keys"`);
   }
 }
