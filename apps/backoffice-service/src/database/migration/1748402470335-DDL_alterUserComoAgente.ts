b17b5d42688d135b610b0400f6331b5a75b5aa5b
Alfred <alfred3698@gmail.com>
2025-06-01 18:57:46 -0600

    V2 se agrega servicio para archivos

--- apps/backoffice-service/src/database/migration/1748402470335-DDL_alterUserComoAgente.ts
+++ apps/backoffice-service/src/database/migration/1748402470335-DDL_alterUserComoAgente.ts
@@ -5,9 +5,6 @@ export class DDLAlterUserComoAgente1748402470335 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "user_v2" ADD "agente_id" uuid`);
-    await queryRunner.query(
-      `ALTER TABLE "user_v2" ADD CONSTRAINT "FK_4c018bc0e30ff8f20653fbdb956" FOREIGN KEY ("agente_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- apps/backoffice-service/src/database/migration/1748703183130-DDL_createAgente.ts
+++ apps/backoffice-service/src/database/migration/1748703183130-DDL_createAgente.ts
@@ -7,10 +7,6 @@ export class DDLCreateAgente1748703183130 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "agentes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "nombre" character varying NOT NULL, "descripcion" character varying, "activo" boolean NOT NULL DEFAULT true, "archivos_allowed" boolean NOT NULL DEFAULT false, "whatsapp_allowed" boolean NOT NULL DEFAULT false, "widget_allowed" boolean NOT NULL DEFAULT false, "ius_gpt_allowed" boolean NOT NULL DEFAULT false, "buscador_allowed" boolean NOT NULL DEFAULT false, "modelo_embedding" character varying, "temperatura" integer NOT NULL DEFAULT '0', "idioma" character varying, "fecha_creacion" TIMESTAMP NOT NULL DEFAULT now(), "modelo_conversacional" character varying, CONSTRAINT "PK_db5be2f9a185bfff6f732bcb924" PRIMARY KEY ("id"))`,
     );
-    await queryRunner.query(`ALTER TABLE "user_v2" ADD "agente_id" uuid`);
-    await queryRunner.query(
-      `ALTER TABLE "user_v2" ADD CONSTRAINT "FK_4c018bc0e30ff8f20653fbdb956" FOREIGN KEY ("agente_id") REFERENCES "agentes"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
